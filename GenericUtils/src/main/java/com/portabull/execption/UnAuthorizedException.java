fcccb1c00b034bc56d307c27348bc6c562909112
portabull <portabullmanagementsystem@gmail.com>
2024-01-13 21:01:21 +0530

    Missing File in previous commit

--- GenericUtils/src/main/java/com/portabull/execption/UnAuthorizedException.java
+++ GenericUtils/src/main/java/com/portabull/execption/UnAuthorizedException.java
@@ -5,5 +5,8 @@ public class UnAuthorizedException extends RuntimeException {
     public UnAuthorizedException(String message) {
         super(message);
     }
+    public UnAuthorizedException() {
+        super("UnAuthorized Access");
+    }
 
 }
