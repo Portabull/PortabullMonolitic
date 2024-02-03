15c667a93eb52a33ee9dc08832ec7dc09f84327c
portabull <portabullmanagementsystem@gmail.com>
2024-02-03 11:53:43 +0530

    Changes in Portabull

--- Services/PortabullService/src/main/java/com/portabull/PortabullService.java
+++ Services/PortabullService/src/main/java/com/portabull/PortabullService.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.ComponentScan;
 public class PortabullService {
 
     public static void main(String[] args) {
+        System.out.println();
         SpringApplication.run(PortabullService.class, args);
     }
 
